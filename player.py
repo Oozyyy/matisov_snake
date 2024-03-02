e557377e9548481e3852203e6be76aa7fe1ccc7b
Oozyyy <151385839+Oozyyy@users.noreply.github.com>
2024-03-02 19:36:57 +0300

    Update player.py

--- player.py
+++ player.py
@@ -1,25 +1,43 @@
 import pygame as pg
 
-
+snake_color = (141, 204, 33)
 
 class Player:
     def __init__(self, screen):
         self.screen = screen
-        self.x = 150
-        self.y = 150
+        self.x = 190
+        self.y = 190
+        self.ver = "DOWN"
     def draw(self):
-        #pg.draw.circle(self.screen, 'white', (self.x, self.y), 10)
-        pg.draw.rect(self.screen, 'White' ,(self.x, self.y, 30, 30))
+        pg.draw.rect(self.screen, snake_color ,(self.x, self.y, 30, 30))
 
     def move(self, keys):
+        if keys[pg.K_s]:
+            self.ver = "DOWN"
+
         if keys[pg.K_w]:
-            self.y -= 10
-        elif keys[pg.K_s]:
-            self.y += 10
-        elif keys[pg.K_a]:
-            self.x -= 10
-        elif keys[pg.K_d]:
-            self.x += 10
-        
+           self.ver = "UP"
+
+        if keys[pg.K_d]:
+           self.ver = "RIGHT"
+    
+        if keys[pg.K_a]:
+           self.ver = "LEFT"
+           
+        match self.ver:
+               case "DOWN":
+                   self.y += 80
+               case "UP":
+                   self.y -= 80
+               case "RIGHT":
+                   self.x += 80
+               case "LEFT":
+                   self.x -= 80
+               
+
+
+            
+                         
+
     def update(self):
-        pass
+        pass  
