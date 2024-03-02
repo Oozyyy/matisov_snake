15de7f06c744ad124798d1d3bfd561431087e36f
Oozyyy <151385839+Oozyyy@users.noreply.github.com>
2024-03-02 19:36:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame as pg
 from player import Player
+from apple import Apple
+
+green = (5, 161, 33)
 
 class Game:
+
     def __init__(self):
         pg.init()
         self.screen = pg.display.set_mode((800, 800))  #screen_size
@@ -9,6 +13,7 @@ class Game:
         self.back_surf = pg.image.load('sources/background.jpg')
         self.clock = pg.time.Clock()
         self.player = Player(self.screen)
+       
         massive = [[0,0,0,0,0,0,0,0,0,0],
                     [0,0,0,0,0,0,0,0,0,0],
                     [0,0,0,0,0,0,0,0,0,0],
@@ -20,16 +25,12 @@ class Game:
                     [0,0,0,0,0,0,0,0,0,0],
                     [0,0,0,0,0,0,0,0,0,0]]
     
-    def draw_lines(self):  
+    def draw_lines (self):  
         for i in range(10):
             
-           
-            pg.draw.line(self.screen, 'white',[0, (i + 1)*80], [800, (i + 1)*80],  3)
-            pg.draw.line(self.screen, 'white', [(i + 1) * 80, 0], [(i+1)* 80, 800], 3)
-       
-           
-            
-
+    
+            pg.draw.line(self.screen, green ,[0, (i + 1)*80], [800, (i + 1)*80],  3)
+            pg.draw.line(self.screen, green, [(i + 1) * 80, 0], [(i+1)* 80, 800], 3)       
 
     def game(self):
         while True:
@@ -45,19 +46,10 @@ class Game:
     def move(self):
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                pg.quit()
-        
+                pg.quit()      
         keys = pg.key.get_pressed()
         self.player.move(keys)
-
     def update(self):
         pg.display.update()
-        
-    
-
-    
-    
-    
-
 game = Game()
 game.game()
