77e5f7bb54d5b4c007be4e34a3ad839f5f871bed
Oozyyy <151385839+Oozyyy@users.noreply.github.com>
2024-04-27 19:39:55 +0300

    Update player.py

--- player.py
+++ player.py
@@ -1,7 +1,4 @@
-import pygame as pg
-
-
-snake_color = (8, 161, 31)
+snake_color = (172, 194, 10)
 
 class Player:
     def __init__(self, screen):
@@ -9,11 +6,25 @@ class Player:
         self.x_player = 85
         self.y_player = 85
         self.ver = "DOWN"
+        self.rect = None
+        self.cound = 0
+        self.last_move = [self.x_player, self.y_player]
+        self.tail = [self.last_move]
+        self.change_block = 0
              
     def draw(self):
         self.player = pg.draw.rect(self.screen, snake_color ,(self.x_player, self.y_player, 30, 30))
+        if len(self.tail) > 1:
+            self.draw_tail()
+            
+    
+    def draw_tail(self):
+        for i in range(len(self.tail)):
+            pg.draw.rect(self.screen, snake_color, [self.tail[i][0],self.tail[i][1],30,30])
 
     def move(self, keys):
+        self.last_move = self.tail[-1]
+        self.tail[0] = [self.x_player, self.y_player]
         if keys[pg.K_s]:
             self.ver = "DOWN"
 
@@ -34,11 +45,15 @@ class Player:
                case "RIGHT":
                    self.x_player += 40
                case "LEFT":
-                   self.x_player -= 40
-    
-  
-
+                   self.x_player -= 40 
+    def update_tail(self):
+        if len(self.tail) > 2:
+            for i in reversed(range(len(self.tail))):
+                self.tail[i] = self.tail[i - 1]
+    def add_block(self):
+        self.tail.append(self.last_move)
 
     def update(self):
-        pass
+        self.update_tail()
+             
              
