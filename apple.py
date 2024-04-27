bd7e3de04716bc19044f5257ec3e98b1e59d6905
Oozyyy <151385839+Oozyyy@users.noreply.github.com>
2024-04-27 19:39:35 +0300

    Update apple.py

--- apple.py
+++ apple.py
@@ -1,28 +1,36 @@
 import pygame as pg
 import random
+from player import Player
 
 
 apple_color = (224, 11, 11)
+dis_width = 800
+dis_height = 800
+snake_block = 20
 
 class Apple:
-    def __init__(self, screen):
+    def __init__(self, screen, player):
         self.screen = screen
-        
-       
-        self.x = random.randint(0,800) 
-        self.y = random.randint(0,800) 
+        self.player = player
+        self.spawn_x = round(random.randrange(0, dis_width - snake_block) / 20.0) * 20.0
+        self.spawn_y = round(random.randrange(0, dis_height - snake_block) / 20.0) * 20.0
+
+    def spawn(self):
+        if self.apple.colliderect(self.player.player):
+            self.spawn_x = round(random.randrange(0, dis_width - snake_block) / 20.0) * 20.0
+            self.spawn_y = round(random.randrange(0, dis_height - snake_block) / 20.0) * 20.0
+            return True
+        return False
 
         
     def draw(self):
-        self.apple = pg.draw.circle(self.screen, apple_color ,(self.x // 40 * 40 + 20, self.y // 40 * 40 + 20),15)
+        #self.apple = pg.draw.rect(self.screen, apple_color ,[self.spawn_x, self.spawn_y, 25, 25 ])
+        
+        self.apple = pg.draw.circle(self.screen, apple_color ,(self.spawn_x, self.spawn_y),15)
         
                       
 
-    def change_pos(self):
-        self.x = random.randint(0,800) 
-        self.y = random.randint(0,800) 
-
-
     def update(self):
-        pass   
+        return self.spawn()
+   
    
