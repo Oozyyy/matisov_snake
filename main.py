7781572ab53019ac5e841c3400055a3805d8c6da
Oozyyy <151385839+Oozyyy@users.noreply.github.com>
2024-05-04 19:42:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from apple import Apple
 green = (7, 130, 25)
 black = (0, 0, 0)
 HEIGHT = 800
-WEIGHT = 800
+WEIGHT = 1200
 game_over =True
 class Game:
 
     def __init__(self):
         pg.init()
-        self.screen = pg.display.set_mode((800, 800))
+        self.screen = pg.display.set_mode((1200, 800))
         self.player = Player(self.screen)
-        self.Apple = Apple(self.screen, self.player)        
+        self.Apple = Apple(self.screen, self.player)         
         self.clock = pg.time.Clock()
            
         massive = [[0,0,0,0,0,0,0,0,0,0],
@@ -29,8 +29,8 @@ class Game:
     
     def draw_lines (self):  
         
-        for i in range(20):
-            pg.draw.line(self.screen, black ,[0, (i + 1)*40], [800, (i + 1)*40],  1)
+        for i in range(30):
+            pg.draw.line(self.screen, black ,[0, (i + 1)*40], [1200, (i + 1)*40],  1)
             pg.draw.line(self.screen, black, [(i + 1) * 40, 0], [(i+1)* 40, 800], 1) 
             
     def game(self):
@@ -46,8 +46,7 @@ class Game:
         self.draw_lines()
         self.player.draw()
         self.Apple.draw()
-    
-             
+                
     def move(self):
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -57,19 +56,14 @@ class Game:
 
     def finish(self):
         return self.player.x_player >= WEIGHT or self.player.x_player < 0 or self.player.y_player >= HEIGHT or self.player.y_player < 0
-    
        
-    def update(self):
+    def update(self):     
         
-        #self.change_pos_apple()
         pg.display.update()
         if self.Apple.update():
             self.player.add_block()
         self.player.update()
-        
-        
-        
+                
 game = Game()
 game.game()
-
 
