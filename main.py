e913f81c875ec0ac1ca5ac6af4460cae4409a7bd
Oozyyy <151385839+Oozyyy@users.noreply.github.com>
2024-05-18 19:39:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import pygame as pg
 from player import Player
 from apple import Apple
+from database import dataBase
 
 green = (7, 130, 25)
 black = (0, 0, 0)
 HEIGHT = 800
 WEIGHT = 1200
 game_over =True
-class Game:
 
-    def __init__(self):
+class Game:
+    def __init__(self, nick_name):
+        self.nickename = nick_name
+        self.database = dataBase()
         pg.init()
         self.screen = pg.display.set_mode((1200, 800))
         self.player = Player(self.screen)
@@ -34,12 +37,14 @@ class Game:
             pg.draw.line(self.screen, black, [(i + 1) * 40, 0], [(i+1)* 40, 800], 1) 
             
     def game(self):
-        while True:
-            while not self.finish():
-                self.draw()
-                self.move()
-                self.update()
-                self.clock.tick(10)
+        while not self.finish():
+            self.draw()
+            self.move()
+            self.update()
+            self.clock.tick(10)
+        self.database.reg(self)
+        self.game_ower_screen(self.database.getDb())
+        self.database.close()
                
     def draw(self):
         self.screen.fill(green)
@@ -63,7 +68,8 @@ class Game:
         if self.Apple.update():
             self.player.add_block()
         self.player.update()
-                
-game = Game()
+
+name = input()              
+game = Game(name)
 game.game()
 
